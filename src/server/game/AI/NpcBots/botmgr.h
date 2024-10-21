734a93c9b941f9c4db78f5865fb0f7f0920e0559
trickerer <onlysuffering@gmail.com>
2024-10-21 19:01:57 +0700

    NPCBots: Add level bracket based multipliers for healing, hp and mp

--- src/server/game/AI/NpcBots/botmgr.h
+++ src/server/game/AI/NpcBots/botmgr.h
@@ -175,6 +175,9 @@ class TC_GAME_API BotMgr
         static PctBrackets GetBotWandererLevelBrackets();
         static float GetBotDamageModByClass(uint8 botclass);
         static float GetBotDamageModByLevel(uint8 botlevel);
+        static float GetBotHealingModByLevel(uint8 botlevel);
+        static float GetBotHPModByLevel(uint8 botlevel);
+        static float GetBotMPModByLevel(uint8 botlevel);
 
         static uint8 GetFollowDistDefault();
         static uint32 GetEngageDelayDPSDefault();
