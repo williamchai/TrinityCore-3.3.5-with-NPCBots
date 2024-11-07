66273f57e0a83e71d7f186688da9084eddcf3765
trickerer <onlysuffering@gmail.com>
2024-11-07 12:16:29 +0700

    NPCBots: Fix Arathi Basin / Eye of the Storm buffs checks

--- src/server/game/AI/NpcBots/bot_ai.cpp
+++ src/server/game/AI/NpcBots/bot_ai.cpp
@@ -19385,7 +19385,7 @@ WanderNode const* bot_ai::GetNextBGTravelNode() const
                     }
                     if (object_type)
                     {
-                        for (uint8 i = 0; i != 2; ++i)
+                        for (uint8 i = 0; i != 3; ++i)
                         {
                             GameObject const* go = ab->GetBGObject(object_type + i);
                             if (go && go->IsInWorld() && go->isSpawned() && go->GetGoState() == GO_STATE_READY)
@@ -19547,7 +19547,7 @@ WanderNode const* bot_ai::GetNextBGTravelNode() const
                     }
                     if (object_type)
                     {
-                        for (uint8 i = 0; i != 2; ++i)
+                        for (uint8 i = 0; i != 3; ++i)
                         {
                             GameObject const* go = ey->GetBGObject(object_type + i);
                             if (go && go->IsInWorld() && go->isSpawned() && go->GetGoState() == GO_STATE_READY)
